d7711ad6acb87434de9652e0b6b3d6bf8a08e2dc
patiesoft <patiesoft@gmail.com>
2022-11-23 17:17:51 +0200

    Working commit

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,9 +1,25 @@
 <template>
-  <v-card class="mx-auto overflow-hidden" height="400">
+  <v-container class="mx-auto" height="400">
     <v-app-bar color="deep-purple" dark app>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title>Tumor-Detect</v-toolbar-title>
+      <v-spacer />
+      <v-menu bottom left offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-avatar dense dark v-bind="attrs" v-on="on" color="grey">{{
+            initials
+          }}</v-avatar>
+        </template>
+        <v-list>
+          <v-list-item :to="{ name: 'profile' }">
+            <v-list-item-title> Profile</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="signout">
+            <v-list-item-title> logout</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" absolute temporary>
@@ -25,15 +41,22 @@
             </v-list-item-icon>
             <v-list-item-title>Account</v-list-item-title>
           </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-book</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Reports</v-list-item-title>
+          </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-  </v-card>
+  </v-container>
 </template>
 <script>
 export default {
   name: "NavBar",
   data: () => ({
+    initials: "PV",
     drawer: false,
     group: null,
   }),
--- src/views/technician/dashboard.vue
+++ src/views/technician/dashboard.vue
@@ -1,12 +1,14 @@
 <template>
   <v-container>
+    <NavBar />
+    <h1 class="text-h3 font-weight-black">Technician</h1>
     <router-view> </router-view>
   </v-container>
 </template>
 <script>
-/* import NavBar from "../../components/navbar.vue"; */
+import NavBar from "../../components/navbar.vue";
 export default {
   name: "TechnicianDashboard",
-  /* components: { NavBar }, */
+  components: { NavBar },
 };
 </script>
